21805553f12f72f9bb7a504867c94fd79237b0f4
Luvas16 <141939109+Luvas16@users.noreply.github.com>
2024-11-25 15:51:53 -0300

    Update TV.js

--- TV.js
+++ TV.js
@@ -1,1 +1,22 @@
+async function Usuarios() {
+  const url = https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json
+  const res = await fetch(url)
+  const dados = await res.json()
+  const Rede = Object.keys(dados)
+  const Usuarios = Object.values(dados)
+  const data = [
+    {
+  x: Redes,
+  y: Usuarios,
+  type:'bar'
+}
+]
+const grafico = document.createElement ('div")
+grafico.className = 'grafico'
 
+document.getelementById('grafico-container').appendChild(grafico)
+Plotly.newplot(grafico, data)
+
+}
+
+Usuarios()
